050c32c7ffdcfb909121ce01ca191e0cff586da6
Przemek Mac <gilewskiprzemek@gmail.com>
2018-12-20 15:28:13 +0100

    validate passwords

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -15,26 +15,51 @@
         }
 
         private function validateUsername($un) {
-            if(strlen($sun) > 25 || strlen($un) < 5) {
+            if(strlen($un) > 25 || strlen($un) < 5) {
                 array_push($this->errorArray, "Your username must be between 6-25 characters");
                 return;
             }
         }
 
-        private function validateFirsteName($fn) {
-            
+        private function validateFirstName($fn) {
+            if(strlen($fn) > 25 || strlen($fn) < 3 ) {
+                array_push($this->errorArray, "Your first name must be between 3-25 characters");
+                return;
+            }
         }
 
         private function validateLastName($ln) {
-            
+            if(strlen($ln) > 25 || strlen($ln) < 3) {
+                array_push($this->errorArray, "Your last name must be between 3-25 characters");
+                return;
+            }
         }
 
         private function validateEmails($em, $em2) {
-            
+            if($em != $em2) {
+                array_push($this->errorArray, "Your emails don't match");
+                return;
+            }
+
+            if(!filter_var($em, FILTER_VALIDATE_EMAIL)){
+                arrya_push($this->errorArray, "Your email is invalid");
+                return;
+            }
         }
 
         private function validatePasswords($pw, $pw2) {
-            
+            if($pw != $pw2) {
+                array_push($this->errorArray, "Your password don't match");
+                return;
+            }
+            if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+                array_push($this->errorArray, "Password can contain only letters and numbers");
+                return;
+            }
+            if(strlen($pw) < 6) {
+                array_push($this->errorArray, "Your password have to be more than 6 characters");
+                return;
+            }
         }
 
 
